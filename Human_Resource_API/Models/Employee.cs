fef9d8262f248d4c7f6353d4db9822b2f3215ff3
varun prakash <varunprakash652@gmail.com>
2024-01-07 22:28:04 +0530

    Added Employee Model

--- Human_Resource_API/Models/Employee.cs
+++ Human_Resource_API/Models/Employee.cs
@@ -2,5 +2,16 @@
 {
     public class Employee
     {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public int DepartmentId { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? TerminationDate { get; set; }
+        public string TerminationReason { get; set; }
+        public bool IsActive { get; set; }
     }
 }
