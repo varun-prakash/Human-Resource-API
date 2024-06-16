01959777afce07a953a7140d151f29d198e1d13e
varun prakash <varunprakash652@gmail.com>
2024-06-16 21:12:42 +0530

    CQRS Implementation/Added Queries

---
+++ Human_Resource_API/Queries/GetEmployeeQuery.cs
@@ -0,0 +1,15 @@
+﻿using Human_Resource_API.Models;
+using MediatR;
+
+namespace Human_Resource_API.Queries
+{
+    public class GetEmployeeQuery : IRequest<Employee>
+    {
+        public int EmployeeId { get; }
+
+        public GetEmployeeQuery(int employeeId)
+        {
+            EmployeeId = employeeId;
+        }
+    }
+}
---
+++ Human_Resource_API/Queries/GetEmployeesQuery.cs
@@ -0,0 +1,10 @@
+﻿using Human_Resource_API.Models;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Human_Resource_API.Queries
+{
+    public class GetEmployeesQuery : IRequest<IEnumerable<Employee>>
+    {
+    }
+}
